65cbcd0a4ff3eb8ec2ea13e36623949d37bbb0fa
Andriy-Lik <106442911+Andriy-Lik@users.noreply.github.com>
2022-08-14 08:50:24 +0000

    Deploying to gh-pages from @ Andriy-Lik/teamFSON59project@752062d42676b2e10a035a478d4276a98886418d 🚀

--- index.456da7db.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMobileMenuBtn:document.querySelector("[data-mobile-menu-open]"),closeMobileMenuBtn:document.querySelector("[data-mobile-menu-close]"),mobileMenu:document.querySelector("[data-mobile-menu]")};function n(){e.mobileMenu.classList.toggle("is-hidden")}e.openMobileMenuBtn.addEventListener("click",n),e.closeMobileMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.456da7db.js.map
--- index.8882b354.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openMobileMenuBtn:document.querySelector("[data-mobile-menu-open]"),closeMobileMenuBtn:document.querySelector("[data-mobile-menu-close]"),mobileMenu:document.querySelector("[data-mobile-menu]")};function n(){e.mobileMenu.classList.toggle("is-hidden")}e.openMobileMenuBtn.addEventListener("click",n),e.closeMobileMenuBtn.addEventListener("click",n)})();
-//# sourceMappingURL=index.8882b354.js.map
